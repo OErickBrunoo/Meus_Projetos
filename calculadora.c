7d263369c73bfac5f7d443d9421a106e0054dd96
OErickBrunoo <erickferreira2729@gmail.com>
2025-09-25 20:54:52 -0300

    Agora com raiz quadrada e correções

--- calculadora.c
+++ calculadora.c
@@ -1,52 +1,61 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     float pnumero, snumero, resultado;
-    char operador;
+    int escolha;
     
+    printf("Selecione a função \n Adição (1)\n subtração (2)\n Multiplicação (3)\n Divisao (4):\n Exponenciação (5) \n Raiz Quadrada (6) \n Resposta:");
+    scanf("%d", &escolha);
     
-    printf("\n Adição (+)\n subtração (-)\n Multiplicação (+)\n Divisao (/):\n Exponenciação (^) \n \n Qual operador:");
-    scanf("%c", &operador);
-    
-    printf("Primeiro número: ");
-    scanf("%f", &pnumero);
-    
-    printf("Segundo número: ");
-    scanf("%f", &snumero);
-    
-    if (operador == '/' && snumero == 0) {
-        printf("operação invalida 1");
-    } else if (operador == '/' && pnumero == 0) {
-        printf("operação invalida 2");
-    } else {
-    switch (operador) {
-        case '+':
+	if (escolha <= 5) {
+		printf("Primeiro número: ");
+		scanf("%f", &pnumero);
+		printf("Segundo número: ");
+		scanf("%f", &snumero);
+		
+		switch (escolha) {
+        case 1:
             resultado = pnumero + snumero;
             printf("\nResultado: %g", resultado);
             break;
-        case '-':
+        case 2:
             resultado = pnumero - snumero;
             printf("\nResultado: %g", resultado);
             break;
-        case '*':
+        case 3:
             resultado = pnumero * snumero;
             printf("\nResultado: %g", resultado);
             break;
-        case '/':
-            resultado = pnumero / snumero;
+        case 4:
+				if (snumero == 0) {
+        printf("operação invalida 1");
+		return 0;
+		} else {
+			resultado = pnumero / snumero;
             printf("\nResultado: %g", resultado);
+		}
             break;
-        case '^':
-            resultado + 1;
-		    for(int i = 1; 1 <= snumero; i++) {
-		    resultado *= pnumero;
-		    }
-		    printf("\nResultado> /%g", resultado);
+        case 5:
+            resultado = pow(pnumero,snumero);
+		    printf("\nResultado: %g", resultado);
 		    break;
+	} else {
+		switch (escolha) {
+        case 6:
+			printf("Digite o Número: ");
+			scanf("%f", &pnumero);
+		if (pnumero >= 0) {
+            resultado = sqrt(pnumero);
+            printf("\nResultado: %g", resultado);
+		} else {
+			printf("Resultado inválido");
+		}
+            break;
         
         default:
             printf("resultado invalido");
     }
-    }
+	}
     return 0;
 }
